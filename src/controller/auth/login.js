03706a0b663bf6f5ec7c58b409ceff936f513091
Rimi <lora3226@inu.ac.kr>
2023-07-21 15:40:57 +0900

    FEAT : 추가 개발 준비 (로그인, 리프레시, 현재 시간 측정)

---
+++ src/controller/auth/login.js
@@ -0,0 +1,56 @@
+import pool from "../../config/database.js";
+import { sign,refresh } from "../../utils/authjwt.js";
+import bcrypt from "bcrypt";
+import { redisClient } from "../../utils/cache.js";
+
+
+export const login = async (req, res) => {
+
+    const { email, password } = req.body;
+
+    const conn = await pool.getConnection();
+
+    const sql = `SELECT user_id, email, password FROM User WHERE email=?`;
+    const [userInfo] = await conn.query(sql, [email])
+
+
+    if (userInfo.length === 0) {
+        return res.status(401).send({
+            ok: false,
+            msg: 'user does not exist'
+        })
+    } else {
+        const user = userInfo[0];
+        const chk = await bcrypt.compare(password, user.password);
+
+
+        if (chk) {
+
+            const accessToken = sign(user);
+            const refreshToken = refresh();
+
+
+            redisClient.set(email, refreshToken, (error, result) => {
+                if (error) {
+                    console.log('Redis set error:', error);
+                } else {
+                    console.log('Redis set result:', result);
+                }
+            });
+
+            return res.status(200).send({
+                ok: true,
+                data: {
+                    accessToken,
+                    refreshToken
+                },
+            });
+        } else {
+            res.status(401).send({
+                ok: false,
+                msg: 'password is incorret. '
+            })
+        }
+    }
+
+}
