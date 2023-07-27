2aff651ce7be92c86fec960411ea68ea395db2bf
Rimi <lora3226@inu.ac.kr>
2023-07-27 17:39:02 +0900

    FEAT : 카카오 로그인 수정

--- src/controller/auth/kakao.js
+++ src/controller/auth/kakao.js
@@ -1,23 +1,26 @@
-import signInKakao from"../../utils/kakao.js";
+import SignInSocial from "../../utils/social.js";
+import axios from "axios";
 
 export const kakao = async (req, res) => {
-    const headers = req.headers["authorization"];
-    
     //테스트용
-    // const headers = "CeIy0ki8IWkb5RDCzhtp4hdjey3c4-OPGsLAUV5pCinJYAAAAYmVpudT";
-    const [accessToken] = await signInKakao(headers);
-    
-    if(accessToken){
-        res.status(200).send({
-            ok: true,
-            accessToken: accessToken,
-        });
-    }else{
-        res.status(404).json({
-            ok: false,
-            msg: ' This E-mail is already taken.'
-        });
-    }
+    const headers = "CeIy0ki8IWkb5RDCzhtp4hdjey3c4-OPGsLAUV5pCinJYAAAAYmVpudT";
+
+    // const headers = req.headers["authorization"];
+    const result = await axios.get("https://kapi.kakao.com/v2/user/me", {
+        headers: {
+            Authorization: `Bearer ${headers}`,
+        }
+    });
+    const { data } = result;
+    const email = data.kakao_account.email;
+
+    const [accessToken] = await SignInSocial('kakao',[email]);
+
+    res.status(200).send({
+        ok: true,
+        accessToken: accessToken,
+    });
+
 
 };
 
---
+++ src/utils/social.js
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken";
+import pool from "../config/database.js";
+import { check_user, social_insert_user, social_select_user, insert_user } from "../dao/auth/signDao.js";
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const SignInSocial = async (which,params) => {
+
+    //외부 API에서 이메일이 안들어올 경우
+    if (!params) throw new error("KEY_ERROR", 400);
+
+    //DB 사용자가 있는지 확인
+    const conn = await pool.getConnection();
+    const [data] = await check_user(conn, params[0]);
+
+    //DB 회원가입 & 사용자가 없을 경우
+    if (!data[0]) {
+        if(which==='kakao'){
+            await social_insert_user(conn, [params[0], 'kakao']);
+        }else if(which==='naver'){
+            await insert_user(conn,params);
+        }
+    }
+
+    const [user] = await social_select_user(conn, params[0]);
+    return [jwt.sign({ user_id: user[0].user_id }, process.env.JWT_SECRET)];
+
+};
+
+export default SignInSocial;
