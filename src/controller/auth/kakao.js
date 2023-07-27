39777430d2000b427f110cf173c4412e0b6ab6c7
Rimi <lora3226@inu.ac.kr>
2023-07-27 17:40:18 +0900

    FEAT: 카카오 로그인 수정

--- src/controller/auth/kakao.js
+++ src/controller/auth/kakao.js
@@ -3,14 +3,16 @@ import axios from "axios";
 
 export const kakao = async (req, res) => {
     //테스트용
-    const headers = "CeIy0ki8IWkb5RDCzhtp4hdjey3c4-OPGsLAUV5pCinJYAAAAYmVpudT";
+    // const headers = "CeIy0ki8IWkb5RDCzhtp4hdjey3c4-OPGsLAUV5pCinJYAAAAYmVpudT";
 
-    // const headers = req.headers["authorization"];
+    const headers = req.headers["authorization"];
     const result = await axios.get("https://kapi.kakao.com/v2/user/me", {
         headers: {
             Authorization: `Bearer ${headers}`,
         }
     });
+
+    //
     const { data } = result;
     const email = data.kakao_account.email;
 
