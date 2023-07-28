a3b182d5adb863d143e587f4e7e090c6bb9da035
Rimi <lora3226@inu.ac.kr>
2023-07-28 14:51:19 +0900

    FIX : 에러수정

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,13 @@
 import express from "express";
-import authRouter from "./auth.js";
+import authRouter from "../router/auth.js"
+
 const router = express.Router();
 
 //auth 
 router.use('/api/user', authRouter);
+
+//my-puppy
+// app.use('/my-puppy', myPuppyRoute);    // 라우트를 추가하고 기본 경로로 /my-puppy 사용
+
 
 export default router;
