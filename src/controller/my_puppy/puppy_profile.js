1e7dd8c940157d863ddd17c0c2d2db8e701ddee8
조하은 <108499120+hani0903@users.noreply.github.com>
2023-08-12 00:59:54 +0900

    Fix
    Fix:  DB select 범위 수정
    MyPuppy DB의 모든 강아지가 아닌 내가 등록한 강아지의 정보만 갖고 오도록 코드 수정

--- src/controller/my_puppy/puppy_profile.js
+++ src/controller/my_puppy/puppy_profile.js
@@ -1,21 +1,18 @@
-import query from "../../mysql/index.js";
-import sql from "../../mysql/sql.js";
 import { puppy_profile } from "../../dao/my_puppy/myPuppyDao.js";
 import pool from "../../config/database.js";
 
 
 export const profile = async(req, res) => {
-    //res.send('내 강아지 메인 페이지');
-    //const puppy = await query('puppyList');
 
     //params
     const conn = await pool.getConnection();
+    const user_id = req.id;
 
     //DB
-    const [myPuppy] = await puppy_profile(conn);
+    const [myPuppy] = await puppy_profile(conn, user_id);
     console.log(myPuppy);
 
-    res.send('메인 페이지');
+    res.send(myPuppy);
 }
 
 export default profile;
