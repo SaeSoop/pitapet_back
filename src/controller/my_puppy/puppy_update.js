1e04e4bdc0a876fc863bd0b058b6e568f8b03a4a
조하은 <108499120+hani0903@users.noreply.github.com>
2023-08-12 01:00:41 +0900

    Feat
    Feat: update 기능 추가
    내 강아지 정보를 수정하는 기능을 추가함

--- src/controller/my_puppy/puppy_update.js
+++ src/controller/my_puppy/puppy_update.js
@@ -1,10 +1,20 @@
-import query from "../../mysql/index.js";
-import sql from "../../mysql/sql.js";
+import { update_puppy } from "../../dao/my_puppy/myPuppyDao.js";
+import pool from "../../config/database.js";
 
 export const update = async(req, res) => {
     
-    //res.send('내 강아지 메인 페이지');
-    res.send('/my-puppy/update-profile 라우트');
+    const dog_id = req.params.itemNumber;
+    const user_id = req.id;
+
+    //params
+    const {name, birth, family, breed, sex, ability, char, game, snack, illness} = req.body;
+    const conn = await pool.getConnection();
+    const params = [ name, birth, family, breed, sex, ability, char, game, snack, illness, dog_id];
+
+    //DB
+    const result = await update_puppy(conn, params);
+
+    res.send(result);
 }
 
 export default update;
