545e9d413dc7e8318525211856ce06b152649b72
조하은 <108499120+hani0903@users.noreply.github.com>
2023-08-12 01:08:56 +0900

    Comment
    Comment: 불필요한 주석 정리
    불필요한 말을 삭제하여 코멘트를 조금 더 간결하게 수정함

--- src/router/my_puppy.js
+++ src/router/my_puppy.js
@@ -6,24 +6,22 @@ import { authJWT } from '../middlewares/auth.js';
 
 const router = express.Router();
 
-//강아지 프로필을 띄우기 위한 라우트
-//app.js에서 기본 경로에 /my-puppy를 사용하기 떄문에 /my-puppy 라우트 경로를 가짐
-router.get('/', profile);
+//강아지 프로필
+//GET: /api/my-puppy/
+router.get('/', authJWT, profile);
 
-//POST /api/my-puppy/create
-//강아지 추가를 위한 라우트
-//app.js에서 기본 경로에 /my-puppy를 사용하기 떄문에 /my-puppy/insert 라우트 경로를 가짐
-router.post('/create', authJWT ,create);
+//강아지 추가
+//POST: /api/my-puppy/create
+router.post('/create', authJWT, create);
 
-//강아지 정보 수정을 위한 라우트
-//app.js에서 기본 경로에 /my-puppy를 사용하기 떄문에 /my-puppy/update-profile 라우트 경로를 가짐
-router.put('/update', update);
+//강아지 정보 수정
+//PUT /api/my-puppy/update
+router.put('/update/:itemNumber', authJWT, update);
 
-//강아지 정보 삭제를 위한 라우트
-//app.js에서 기본 경로에 /my-puppy를 사용하기 떄문에 /my-puppy/delete 라우트 경로를 가짐
+//강아지 정보 삭제
+//DELETE: /api/my-puppy/delete
 // router.delete('/delete', function(req, res) {
 //     res.send('/my-puppy/delete 라우트');
 // });
 
-//module.exports = router;
 export default router;
