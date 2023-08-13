593886cd95fe7656fa6db2c8cf2368219f91e6ad
조하은 <108499120+hani0903@users.noreply.github.com>
2023-08-13 23:58:01 +0900

    🎨Style
    Style: 변수 이름 변경
    DB의 각 속성 값과 동일하게 변수 이름을 변경하였음

--- src/controller/my_puppy/puppy_create.js
+++ src/controller/my_puppy/puppy_create.js
@@ -1,11 +1,26 @@
 import { insert_puppy } from "../../dao/my_puppy/myPuppyDao.js";
 import pool from "../../config/database.js";
+import getPropertyPath from "../../utils/getPropertyPath.js";
+import multer from "multer";
+import path from "path";
+import storage from "../../utils/storage.js";
+
+
+const upload = multer({storage: storage}); //multer 객체 생성
+
 
 export const create = async (req, res) => {
 
-    const { name, birth, family, breed, sex, ability, char, game, snack, illness } = req.body;
+    const path = req.file.path;
+    const dog_image = path.replace(/\\/g, "/");
+    console.log("path: ", path);
+
+    //const dog_image = getPropertyPath(req.file.path);
+    //console.log("dog_image: ", dog_image);
+
+    const { dog_name, dog_birth, dog_family, dog_breed, dog_sex, dog_ability, dog_char, dog_game, dog_snack, dog_illness } = req.body;
     const user_id = req.id;
-    const params = [ name, birth, family, breed, sex, ability, char, game, snack, illness, user_id];
+    const params = [ dog_image, dog_name, dog_birth, dog_family, dog_breed, dog_sex, dog_ability, dog_char, dog_game, dog_snack, dog_illness, user_id];
 
     //params
     const conn = await pool.getConnection();
@@ -13,8 +28,6 @@ export const create = async (req, res) => {
     //DB
     const [myPuppy] = await insert_puppy(conn, params);
 
-    
-    
     res.status(200).send({
         success: true,
         msg: "강아지 추가 완료",
