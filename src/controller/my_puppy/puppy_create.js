c714b12491418ffa216e17d02ed8807154435236
Rimi <lora3226@inu.ac.kr>
2023-08-14 15:38:50 +0900

    Fix : puppy_create.js 불필요한 주석 제거

--- src/controller/my_puppy/puppy_create.js
+++ src/controller/my_puppy/puppy_create.js
@@ -12,12 +12,7 @@ const upload = multer({storage: storage}); //multer 객체 생성
 export const create = async (req, res) => {
 
     const path = req.file.path;
-    const dog_image = path.replace(/\\/g, "/");
-    console.log("path: ", path);
-
-    //const dog_image = getPropertyPath(req.file.path);
-    //console.log("dog_image: ", dog_image);
-
+    const dog_image = path.replace(/\\/g, "/");    
     const { dog_name, dog_birth, dog_family, dog_breed, dog_sex, dog_ability, dog_char, dog_game, dog_snack, dog_illness } = req.body;
     const user_id = req.id;
     const params = [ dog_image, dog_name, dog_birth, dog_family, dog_breed, dog_sex, dog_ability, dog_char, dog_game, dog_snack, dog_illness, user_id];
@@ -32,7 +27,6 @@ export const create = async (req, res) => {
         success: true,
         msg: "강아지 추가 완료",
     });
-    //res.status(400).send('Bad Request');
 };
 
 export default create;
