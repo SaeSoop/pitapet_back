e4d57806d4d64bc72dca8a357399e9cbfa023ed9
조하은 <108499120+hani0903@users.noreply.github.com>
2023-08-19 21:00:11 +0900

    📰Docs
    Docs: answer와 관련된 라우터 추가
    answer와 관련된 요청을 처리하기 위해 코드를 작성함.

---
+++ src/router/answer.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import update from "../controller/my_puppy/puppy_update.js";
+import create from "../controller/answer/answer_create.js";
+import { authJWT } from '../middlewares/auth.js';
+
+const router = express.Router();
+
+//질문 읽어오기
+//GET: /api/answer/read
+//router.get('/read', read);
+
+//답변 추가
+//POST: /api/answer/create
+router.post('/create', authJWT, create);
+
+
+export default router;
