ba9b2c1bf38602fd8d695eaa53911e16ed68a2bc
조하은 <108499120+hani0903@users.noreply.github.com>
2023-08-19 21:02:37 +0900

    🎨Comment
    Comment: 필요없는 주석 삭제
    필요없는 주석을 삭제함

--- src/dao/question/questionDao.js
+++ src/dao/question/questionDao.js
@@ -1,8 +1,7 @@
-export const read_question = async (conn) => {
-    const questionList = `select * from Question`;
+export const read_question = async (conn, params) => {
+    const questionList = `select * from Question WHERE question_id=?`;
 
-    //await conn.query(puppyList_query, params);
-    const [question] = await conn.query(questionList);
+    const [question] = await conn.query(questionList, params);
 
     return [question];
 };
@@ -14,21 +13,10 @@ export const insert_question = async (conn, params) => {
         VALUES (?);
     `;
 
-    //await conn.query(puppyList_query, params);
     const [newQuestion] = await conn.query(questionInsert, params);
 
     return [newQuestion];
 };
-
-// export const insert_question = async (conn, params) => {
-
-//     const questionInsert = `INSERT INTO Question (content) VALUES (?)`;
-
-//     //await conn.query(puppyList_query, params);
-//     const [newQues] = await conn.query(questionInsert,"강아지와 처음 만난 날 어떤 감정을 느꼈나요?");
-
-//     return [newQues];
-// };
 
 export const update_question = async (conn, params) => {
     
@@ -38,20 +26,7 @@ export const update_question = async (conn, params) => {
         WHERE question_id = ?;
     `;
 
-    //await conn.query(puppyList_query, params);
     const [updatedQuestion] = await conn.query(questionUpdate, params);
 
     return [updatedQuestion];
 };
-
-// export const delete_question = async (conn, params) => {
-    
-//     const puppyDelete = `
-//         delete from MyPuppy where dog_id=?
-//     `;
-
-//     //await conn.query(puppyList_query, params);
-//     const result = await conn.query(puppyDelete, params);
-
-//     return result;
-// };
