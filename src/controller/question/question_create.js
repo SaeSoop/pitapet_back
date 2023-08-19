c128371d1e0f0fac77713b7d9d8514492e78077d
조하은 <108499120+hani0903@users.noreply.github.com>
2023-08-19 12:02:24 +0900

    🚑Fixes
    Fixes: 에러 수정 중
    Insert문이 제대로 실행이 안됨

---
+++ src/controller/question/question_create.js
@@ -0,0 +1,22 @@
+import { insert_question } from "../../dao/question/questionDao.js";
+import pool from "../../config/database.js";
+
+export const create = async (req, res) => {
+
+    const { content } = req.body;
+    const params = [content];
+
+    const conn = await pool.getConnection();
+
+    //DB
+    const [newQuestion] = await insert_question(conn, params);
+
+    conn.release();
+    
+    res.status(200).send({
+        success: true,
+        msg: "질문 추가 완료",
+    });
+};
+
+export default insert_question;
