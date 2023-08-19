e81bda518d80f12bd83b87710009e34e034dcb70
조하은 <108499120+hani0903@users.noreply.github.com>
2023-08-19 20:58:37 +0900

    🎨Comment
    Comment: 불필요한 주석 삭제
    가독성을 높이기 위해 필요없는 주석을 삭제함

--- src/dao/my_puppy/myPuppyDao.js
+++ src/dao/my_puppy/myPuppyDao.js
@@ -1,7 +1,6 @@
 export const puppy_profile = async (conn, params) => {
     const puppyList = `select * from MyPuppy WHERE user_id = ?`;
 
-    //await conn.query(puppyList_query, params);
     const [puppy] = await conn.query(puppyList, params);
 
     return [puppy];
@@ -11,7 +10,6 @@ export const insert_puppy = async (conn, params) => {
     const puppyInsert = `INSERT INTO MyPuppy (dog_image, dog_name, dog_birth, dog_family, dog_breed, dog_sex, dog_ability, dog_char,
         dog_game, dog_snack, dog_illness, user_id) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)`;
 
-    //await conn.query(puppyList_query, params);
     const [newPuppy] = await conn.query(puppyInsert, params);
 
     return [newPuppy];
@@ -26,7 +24,6 @@ export const update_puppy = async (conn, params) => {
         WHERE dog_id = ?;
     `;
 
-    //await conn.query(puppyList_query, params);
     const [updatedPuppy] = await conn.query(puppyUpdate, params);
 
     return [updatedPuppy];
@@ -38,7 +35,6 @@ export const delete_puppy = async (conn, params) => {
         delete from MyPuppy where dog_id=?
     `;
 
-    //await conn.query(puppyList_query, params);
     const result = await conn.query(puppyDelete, params);
 
     return result;
