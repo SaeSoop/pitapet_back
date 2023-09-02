3548de4b9c898dcc447e645df5e17663604db81a
조하은 <108499120+hani0903@users.noreply.github.com>
2023-09-02 16:01:56 +0900

    Feat: 총 일기 개수 읽어오는 기능 추가
    작성한 일기의 개수를 가져오는 기능을 추가함

--- src/router/diary.js
+++ src/router/diary.js
@@ -1,5 +1,6 @@
 import express from "express";
 import create from "../controller/diary/diary_create.js";
+import read from "../controller/diary/diary_read.js";
 import update from "../controller/diary/diary_update.js";
 import open from "../controller/diary/diary_open.js";
 import like from "../controller/diary/diary_like.js";
@@ -7,6 +8,9 @@ import { authJWT } from '../middlewares/auth.js';
 import storage from "../utils/storage.js";
 import multer from "multer";
 const router = express.Router();
+
+//읽기 전체 읽어오기
+router.get('/read', read);
 
 // 일기 등록하기
 // POST: /api/diary/create
